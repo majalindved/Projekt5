b15570c6c359060794aaae2d82cc5155e2c764d0
hp.gammeltoft@hotmail.com <96HaPEGa96>
2020-04-15 09:23:43 +0200

    Kode til indkøbsliste
    Javascript til indkøbsliste.
    
    CSS til børnemode
    
    HTML til indkøbsliste og produkter.

---
+++ app/ikoner.js
@@ -0,0 +1,20 @@
+function openTab (evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+    const resultsList = document.getElementById('results')
+    new URLSearchParams(window.location.search).forEach((value, name) => {
+        resultsList.append(`${name} ${value}`)
+        resultsList.append(document.createElement('br'))
+})
+
