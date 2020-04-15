b1ba5be8d06d5f84071c214d5921ac2406bbceb9
AnnCathrine23 <58133322+AnnCathrine23@users.noreply.github.com>
2020-04-16 00:54:01 +0200

    prøve

--- app/store.js
+++ app/store.js
@@ -1,3 +1,7 @@
+function goBack() {
+  window.history.back();
+}
+
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready)
 } else {
@@ -143,7 +147,7 @@ function openTab (evt, tabName) {
 }
 
 function swapStyleSheet (sheet){
-   document.getElementById('pagestyle').setAttribute('href', sheet); 
+   document.getElementById('pagestyle').setAttribute('href', sheet);
 }
 
 window.addEventListener('mouseup', function(){
@@ -151,13 +155,12 @@ window.addEventListener('mouseup', function(){
     if(event.target != box) {
     box.style.display = 'none';
    }
-})  
+})
 
 document.getElementById("Mydiv").addEventListener("click", function(){
     let t1, div1, temp, div2
     t1 = document.getElementById('target1');
-    
+
     div1 = document.querySelector('#indkoebsliste');
         t1.appendChild(div1.cloneNode(true) );
 })
-
